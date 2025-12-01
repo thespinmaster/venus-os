08e402a4f5448783aa25acb341ed1a4657919b51
thespinmaster <nealandrews@outlook.com>
2025-12-01 00:31:00 +0000

    updated installer

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 
+
+if [[ -n "$1" ]]; then
+  build=all
+else
+ build="$1"
+fi
+
 read -p "comment:" COMMENT
 
 if [[ -z "${COMMENT}" ]]; then
   COMMENT="updated installer"
 fi
 
+make $build
 git commit -am "${COMMENT}"
 sudo git push
--- utils/opkg-helpers/src/data/opkg-helpers/opkg-common.sh
+++ utils/opkg-helpers/src/data/opkg-helpers/opkg-common.sh
@@ -108,9 +108,10 @@ ensure_installed() {
   while IFS= read -r line
   do 
     # ignore comments
-    if [[ -z grep '^[[:blank:]]*[^[:blank:]#;]' <<< "${line}"  ]]; then
+    if [[ -z $(grep '^[[:blank:]]*[^[:blank:]#]' <<< "${line}" ) ]]; then
       continue
     fi
+
     if [[ -n "$line" ]]; then
       try_install_package "$line" 
     fi
